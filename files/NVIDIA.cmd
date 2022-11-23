f81c8aa74de6d800f72c6edd73c3b244867c3b3c
SULFURA <isulfurax@gmail.com>
2022-11-23 20:30:53 +0100

    NVIDIA

--- files/NVIDIA.cmd
+++ files/NVIDIA.cmd
@@ -3,23 +3,25 @@ color 03
 Mode 128,29
 C:
 cd "C:\Users\%username%\Documents\SULFURAX\SCZOptimizer"
-
 for /f %%i in ('Reg query "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}" /t REG_SZ /s /e /f "NVIDIA" ^| findstr "HKEY"') do (
 	Reg add "%%a" /v "PowerMizerEnable" /t REG_DWORD /d "1" /f >nul 2>&1
 	Reg add "%%a" /v "PowerMizerLevel" /t REG_DWORD /d "1" /f >nul 2>&1
 	Reg add "%%a" /v "PowerMizerLevelAC" /t REG_DWORD /d "1" /f >nul 2>&1
 	Reg add "%%a" /v "PerfLevelSrc" /t REG_DWORD /d "8738" /f >nul 2>&1
-    
 for /f %%a in ('Reg query "HKLM\System\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}" /t REG_SZ /s /e /f "NVIDIA" ^| findstr "HKEY"') do (
 	Reg add "%%a" /v "RMHdcpKeyglobZero" /t REG_DWORD /d "1" /f
 )
-
+for /f %%a in ('Reg query "HKLM\System\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}" /t REG_SZ /s /e /f "NVIDIA" ^| findstr "HKEY"') do (
+    Reg add "%%a" /v "EnableTiledDisplay" /t REG_DWORD /d "0" /f
+    Reg add "%%a" /v "TCCSupported" /t REG_DWORD /d "0" /f
+)
+Reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm\FTS" /v "EnableRID61684" /t REG_DWORD /d "1" /f
+Reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm" /v "DisableWriteCombining" /t Reg_DWORD /d "1" /f
 Reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm" /v "DisablePreemption" /t Reg_DWORD /d "1" /f
 Reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm" /v "DisableCudaContextPreemption" /t Reg_DWORD /d "1" /f
 Reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm" /v "EnableCEPreemption" /t Reg_DWORD /d "0" /f
 Reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm" /v "DisablePreemptionOnS3S4" /t Reg_DWORD /d "1" /f
 Reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm" /v "ComputePreemption" /t Reg_DWORD /d "0" /f
-
 rmdir /S /Q "C:\Users\%username%\Documents\SULFURAX\SCZOptimizer\nvidiaProfileInspector"
 curl -g -L -# -o "C:\Users\%username%\Documents\SULFURAX\SCZOptimizer\nvidiaProfileInspector.zip" "https://github.com/Orbmu2k/nvidiaProfileInspector/releases/latest/download/nvidiaProfileInspector.zip"
 powershell -NoProfile Expand-Archive 'C:\Users\%username%\Documents\SULFURAX\SCZOptimizer\nvidiaProfileInspector.zip' -DestinationPath 'C:\Users\%username%\Documents\SULFURAX\SCZOptimizer\nvidiaProfileInspector\'
@@ -27,7 +29,6 @@ del /F /Q "C:\Users\%username%\Documents\SULFURAX\SCZOptimizer\nvidiaProfileInsp
 curl -g -L -# -o "C:\Users\%username%\Documents\SULFURAX\SCZOptimizer\nvidiaProfileInspector\Profile.nip" "https://raw.githubusercontent.com/SULFURA/SCZOptimizer/main/files/Profile.nip"
 cd "C:\Users\%username%\Documents\SULFURAX\SCZOptimizer\nvidiaProfileInspector\"
 nvidiaProfileInspector.exe "Profile.nip" 
-
 Reg add "HKLM\SOFTWARE\NVIDIA Corporation\NvControlPanel2\Client" /v "OptInOrOutPreference" /t REG_DWORD /d 0 /f >nul 2>&1
 Reg add "HKLM\SOFTWARE\NVIDIA Corporation\Global\FTS" /v "EnableRID44231" /t REG_DWORD /d 0 /f >nul 2>&1
 Reg add "HKLM\SOFTWARE\NVIDIA Corporation\Global\FTS" /v "EnableRID64640" /t REG_DWORD /d 0 /f >nul 2>&1
@@ -37,21 +38,12 @@ schtasks /change /disable /tn "NvTmRep_CrashReport1_{B2FE1952-0186-46C3-BAEC-A80
 schtasks /change /disable /tn "NvTmRep_CrashReport2_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8}" >nul 2>&1
 schtasks /change /disable /tn "NvTmRep_CrashReport3_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8}" >nul 2>&1
 schtasks /change /disable /tn "NvTmRep_CrashReport4_{B2FE1952-0186-46C3-BAEC-A80AA35AC5B8}" >nul 2>&1
-
 Reg add "HKCU\Software\NVIDIA Corporation\Global\NVTweak\Devices\509901423-0\Color" /v "NvCplUseColorCorrection" /t Reg_DWORD /d "0" /f
 Reg add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "PlatformSupportMiracast" /t Reg_DWORD /d "0" /f
 Reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm\Global\NVTweak" /v "DisplayPowerSaving" /t Reg_DWORD /d "0" /f
 cd "%SystemDrive%\Program Files\NVIDIA Corporation\NVSMI\"
 nvidia-smi -acp UNRESTRICTED >nul 2>&1
 nvidia-smi -acp DEFAULT >nul 2>&1
-for /f %%a in ('Reg query "HKLM\System\CurrentControlSet\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}" /t REG_SZ /s /e /f "NVIDIA" ^| findstr "HKEY"') do (
-Reg add "%%a" /v "EnableTiledDisplay" /t REG_DWORD /d "0" /f
-Reg add "%%a" /v "TCCSupported" /t REG_DWORD /d "0" /f
-)
-Reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm\FTS" /v "EnableRID61684" /t REG_DWORD /d "1" /f
-) >nul 2>&1
-
-Reg add "HKLM\SYSTEM\CurrentControlSet\Services\nvlddmkm" /v "DisableWriteCombining" /t Reg_DWORD /d "1" /f
 
 :: Clear CMD
 timeout /t 5 /nobreak
